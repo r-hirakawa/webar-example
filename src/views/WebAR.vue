611cfb7b0ed9aa1b82adc317f6974fc612af300f
r-hirakawa <r-hirakawa@xr.jp.nec.com>
2021-01-09 16:40:16 +0900

    Vuetify の組み込みと A-Frame を動作させるための回避策の適用

--- src/views/WebAR.vue
+++ src/views/WebAR.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <a-scene
-      id="scene"
       vr-mode-ui="enabled: false;"
       loading-screen="enabled: true;"
       arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: true;"
       embedded
+      id="scene"
     >
 
       <!-- a-assets: preload/cache するアセットの定義 -->
@@ -77,26 +77,46 @@ export default {
   },
   methods: {
   },
-  // Vue コンポーネントから離れるとき、A-Frame コンテンツをアンロードする.
-  // https://router.vuejs.org/ja/guide/advanced/navigation-guards.html#%E3%82%B3%E3%83%B3%E3%83%9D%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E5%86%85%E3%82%AC%E3%83%BC%E3%83%89
+  mounted () {
+    // [Workaround] <video> 要素から z-index を削除
+    setTimeout(() =>
+      {
+        var arVideo = document.querySelector('#arjs-video');
+        if (arVideo) {
+          var style = arVideo.getAttribute('style');
+          arVideo.setAttribute('style', style.replace('z-index: -2;', ''));
+          console.info('[workaround] delete z-index from <video id="arjs-video">')
+        }
+      }
+      ,3000
+    );
+  },
+  /**
+   * [Workaround]
+   *  概要   : Vue Router で A-Frame の Vue コンポーネントから離れてもカメラ+AR映像が表示されたままになる.
+   *  原因   : A-Frame が Vue.js の制御外で <body> 配下に <video> 要素を挿入するため Vue コンポーネントと一緒にアンロードされない.
+   *  回避策 : Router イベントでコンポーネントから離れるときに <video> 要素を DOM 上から削除する.
+   *  補足   : <video> 以外に挿入される要素も一緒にクリアする必要がある.
+   * @see https://router.vuejs.org/ja/guide/advanced/navigation-guards.html#%E3%82%B3%E3%83%B3%E3%83%9D%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E5%86%85%E3%82%AC%E3%83%BC%E3%83%89
+   */
   beforeRouteLeave (to, from, next) {
     // <a-scene> の再生を停止
     var scene = document.querySelector('a-scene');
     if (scene) {
       scene.pause();
-      console.log('AR Scene paused.');
+      console.info('[workaround] <a-scene> paused')
     }
     // <video> 要素の削除
     var arVideo = document.querySelector('#arjs-video');
     if (arVideo) {
       arVideo.remove();
-      console.log('AR <video> element removed.');
+      console.info('[workaround] delete element <video id="arjs-video">')
     }
     // DebugUI 要素の削除
     var arDebugUI = document.querySelector('#arjsDebugUIContainer');
     if (arDebugUI) {
       arDebugUI.remove();
-      console.log('AR DebugUI removed.');
+      console.info('[workaround] delete element <div id="arjsDebugUIContainer">')
     }
     next();
   },
@@ -105,12 +125,38 @@ export default {
 
 <style>
 /**
- * [Workaround] Vue.js で <a-seene> がレンダリングされない問題.
- * @see https://note.com/saifunakusu/n/n3988595305b9
- *
- *  - <a-scene> の上位要素に対して height: 100% を指定する.
+ * [Workaround]
+ *  概要   : A-Frame を Vue.js に組み込むと <a-scene> がレンダリングされなくなる.
+ *  原因   : <a-scene> のスタイルが heigth: 0 となっている.
+ *  回避策 : <a-scene> の上位要素に対して height: 100% を指定する.
+ *  補足   : Vuetify 適用時の上位要素(#app, .v-application--wrap, .v-main, .v-main__wrap)はデフォルトで height: 100% となるため不要.
  */
-#app, .container {
+.container {
   height: 100%;
 }
+
+/**
+ * [Workaround]
+ *  概要   : A-Frame を導入したアプリケーションに Vuetify を組み込むと <video> と <a-scene> がレンダリングされなくなる.
+ *  原因   : A-Frame と Vuetify の間で z-index スタイルの利用指針における不整合が発生する.
+ *  回避策 : <video> と <a-scene> が Vuetify の要素より前段に表示されるよう z-index を調整する.
+ *          <video> 要素は Vue.js のライフサイクルイベント外で A-Frame により直接 DOM に挿入されるため updated から timer を起動して削除を図る...
+ *  補足   : z-index 値の状態と変更方針
+ *  ------------------------------
+ *  A-Frame
+ *    z-index
+ *         -2 : <video id="arjs-video"> -> 0 に変更
+ *          0 : <a-scene>               -> 1 に変更
+ *
+ *  Vuetify
+ *    z-index
+ *          0 : <v-main>
+ *          0 : <v-bottom-navigation>   -> 4 に変更
+ *          5 : <v-app-bar>
+ *  ------------------------------
+ */
+#scene {
+  z-index: 1;
+}
+
 </style>
